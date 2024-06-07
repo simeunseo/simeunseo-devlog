d0d36d79d4c4c7b522c76d5944cc44c0ad181831
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-06-07 23:22:15 +0900

    fix: 프로필영역 스크롤 변경

--- src/routes/Feed/index.tsx
+++ src/routes/Feed/index.tsx
@@ -1,15 +1,14 @@
-import { useState } from "react"
-
-import SearchInput from "./SearchInput"
+import ContactCard from "./ContactCard"
 import { FeedHeader } from "./FeedHeader"
 import Footer from "./Footer"
-import styled from "@emotion/styled"
-import TagList from "./TagList"
 import MobileProfileCard from "./MobileProfileCard"
-import ProfileCard from "./ProfileCard"
-import ServiceCard from "./ServiceCard"
-import ContactCard from "./ContactCard"
 import PostList from "./PostList"
+import ProfileCard from "./ProfileCard"
+import SearchInput from "./SearchInput"
+import ServiceCard from "./ServiceCard"
+import TagList from "./TagList"
+import styled from "@emotion/styled"
+import { useState } from "react"
 
 const HEADER_HEIGHT = 73
 
@@ -120,7 +119,6 @@ const StyledWrapper = styled.div`
     }
 
     display: none;
-    overflow: scroll;
     position: sticky;
     top: ${HEADER_HEIGHT - 10}px;
 
